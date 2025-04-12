f9c6a8c35fba42a913e4ac8cfbdf415cdce1ba1e
MariusMitrikevicius <zandenis@gmail.com>
2025-04-12 15:50:19 +0300

    Initial commit

---
+++ src/main/java/lt/mariaus/darbas/DarbasApplication.java
@@ -0,0 +1,13 @@
+package lt.mariaus.darbas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarbasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarbasApplication.class, args);
+    }
+
+}
